5e64608a5a390dc625bcab9b32fae65588f31430
Danae <savvididanae@gmail.com>
2020-07-27 19:09:41 +0300

    added second view controller

--- xcode_proj/second_view_controller.swift
+++ xcode_proj/second_view_controller.swift
@@ -1,163 +1,101 @@
 //
-//  ViewController.swift
-//  cell4
+//  SecondViewController.swift
+//  cell3
 //
-//  Created by dsav on 7/24/20.
+//  Created by dsav on 7/27/20.
 //  Copyright © 2020 dsav. All rights reserved.
 //
+
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class SecondViewController: UIViewController {
 
-    
-    @IBOutlet weak var number1: UITextField!
-    @IBOutlet weak var number2: UITextField!
-    @IBOutlet weak var number3: UITextField!
-    @IBOutlet weak var number4: UITextField!
-    @IBOutlet weak var number5: UITextField!
-    @IBOutlet weak var number6: UITextField!
-    @IBOutlet weak var number7: UITextField!
-    @IBOutlet weak var number8: UITextField!
-    @IBOutlet weak var dilution: UITextField!
+
     @IBOutlet weak var wells: UITextField!
-    @IBOutlet weak var volwell: UITextField!
-    
-    
-    @IBOutlet weak var calcbtn: UIButton!
-    @IBOutlet weak var finalAns: UIButton!
-    
+    @IBOutlet weak var volwel: UITextField!
     
     @IBOutlet weak var ans: UITextView!
-    @IBOutlet weak var finalOut: UITextView!
+    @IBOutlet weak var finalAns: UITextView!
     
+    @IBOutlet weak var finalCalc: UIButton!
+    
+    var n1 = 6
+    var n2 = 6
+    var n3 = 6
+    var n4 = 6
+    var n5 = 6
+    var n6 = 6
+    var n7 = 6
+    var n8 = 6
+    var dil = 6
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        calcbtn.layer.cornerRadius = 10
-        calcbtn.clipsToBounds = true
-        finalAns.layer.cornerRadius = 10
-        finalAns.clipsToBounds = true
-        ans.layer.cornerRadius = 10
-        ans.clipsToBounds = true
-        finalOut.layer.cornerRadius = 10
-        finalOut.clipsToBounds = true
-        
+        //TOOLBAR COMMANDS
         let toolBar = UIToolbar()
         
         toolBar.sizeToFit()
         
+        //DONE BUTTON ON TOOLBAR
         let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(self.doneClicked))
             
             toolBar.setItems([doneButton], animated: false)
         
-        wells.keyboardType = UIKeyboardType.numberPad
-        dilution.keyboardType = UIKeyboardType.numberPad
-        volwell.keyboardType = UIKeyboardType.numberPad
-        number1.keyboardType = UIKeyboardType.numberPad
-        number2.keyboardType = UIKeyboardType.numberPad
-        number3.keyboardType = UIKeyboardType.numberPad
-        number4.keyboardType = UIKeyboardType.numberPad
-        number5.keyboardType = UIKeyboardType.numberPad
-        number6.keyboardType = UIKeyboardType.numberPad
-        number7.keyboardType = UIKeyboardType.numberPad
-        number8.keyboardType = UIKeyboardType.numberPad
-        
         wells.inputAccessoryView = toolBar
-        dilution.inputAccessoryView = toolBar
-        volwell.inputAccessoryView = toolBar
-        number1.inputAccessoryView = toolBar
-        number2.inputAccessoryView = toolBar
-        number3.inputAccessoryView = toolBar
-        number4.inputAccessoryView = toolBar
-        number5.inputAccessoryView = toolBar
-        number6.inputAccessoryView = toolBar
-        number7.inputAccessoryView = toolBar
-        number8.inputAccessoryView = toolBar
+        volwel.inputAccessoryView = toolBar
         
-        wells.delegate = self
-        dilution.delegate = self
-        volwell.delegate = self
-        number1.delegate = self
-        number2.delegate = self
-        number3.delegate = self
-        number4.delegate = self
-        number5.delegate = self
-        number6.delegate = self
-        number7.delegate = self
-        number8.delegate = self
+        //-----------------------------------------------
         
-    }
-
-    @IBAction func calc(_ sender: Any) {
-        let numb1 = Int(number1.text!)
-        let numb2 = Int(number2.text!)
-        let numb3 = Int(number3.text!)
-        let numb4 = Int(number4.text!)
-        let numb5 = Int(number5.text!)
-        let numb6 = Int(number6.text!)
-        let numb7 = Int(number7.text!)
-        let numb8 = Int(number8.text!)
-            
-        let summ = Int(numb1! + numb2! + numb3! + numb4! + numb5! + numb6! + numb7! + numb8!)
-        let aver = Int(summ / 8)
-            
+        //calculating average
+        let aver = Int((n1+n2+n3+n4+n5+n6+n7+n8)/8)
+        
         //stalo = Cells/ml
-        let stalo = Int(aver * 5 * 10000)
+        let stalo = Int(aver * dil * 10000)
         
         ans.text = "\(stalo) Cells/ml"
+        
+        finalCalc.layer.cornerRadius = 10
+        finalCalc.clipsToBounds = true
+        //well.layer.cornerRadius = 10
+        //well.clipsToBounds = true
+        
+        //well.keyboardType = UIKeyboardType.numberPad
+        volwel.keyboardType = UIKeyboardType.numberPad
+        
+        
     }
     
-    
-    @IBAction func finalAns(_ sender: Any) {
-        let numb1 = Int(number1.text!)
-        let numb2 = Int(number2.text!)
-        let numb3 = Int(number3.text!)
-        let numb4 = Int(number4.text!)
-        let numb5 = Int(number5.text!)
-        let numb6 = Int(number6.text!)
-        let numb7 = Int(number7.text!)
-        let numb8 = Int(number8.text!)
-        let wellsnumb = Int(wells.text!)
-        let volwe = Int(volwell.text!)
-        let dil = Int(dilution.text!)
+    @IBAction func calcFinal(_ sender: Any) {
+        let well = Int(wells.text!)
+        let volwell = Int(volwel.text!)
         
-        let summ = Int(numb1! + numb2! + numb3! + numb4! + numb5! + numb6! + numb7! + numb8!)
-        let aver = Int(summ / 8)
+        //calculating average
+        let aver = Int((n1+n2+n3+n4+n5+n6+n7+n8)/8)
         
-        //stalo = Cells/ml total
-        let stalo = Int(aver * 5 * 10000)
+        //stalo = cells/ml
+        let stalo = Int(aver * dil * 10000)
         
         //stalo2 = Microliters of Cells
-        let stalo21 = Int(wellsnumb! * dil!)
+        let stalo21 = Int(well! * dil)
         let stalo22 = Int(stalo21 * 1000000)
         
         let stalo2 = Int(stalo22 / stalo)
         
         //Microliters of DMEM
-        let stalo3 = Int(wellsnumb! * volwe! - stalo2)
+        let stalo3 = Int(well! * volwell! - stalo2)
         
-        finalOut.text = "\(stalo) Cells/ml total \n\(stalo2) Microliters of Cells \n\(stalo3) Microliters of DMEM"
+        finalAns.text = "\(stalo) Cells/ml total \n\(stalo2) Microliters of Cells \n\(stalo3) Microliters of DMEM"
     }
-    
-    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         wells.resignFirstResponder()
-        volwell.resignFirstResponder()
-        number1.resignFirstResponder()
-        number2.resignFirstResponder()
-        number3.resignFirstResponder()
-        number4.resignFirstResponder()
-        number5.resignFirstResponder()
-        number6.resignFirstResponder()
-        number7.resignFirstResponder()
-        number8.resignFirstResponder()
+        volwel.resignFirstResponder()
         return true
     }
     
     @objc func doneClicked() {
         view.endEditing(true)
     }
-    
+
 }
